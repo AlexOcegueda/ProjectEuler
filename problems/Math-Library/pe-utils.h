54f275b7dd8eea5f18fbb3a0aed72bf62c83b068
Alex Ocegueda <alexocegueda59@gmail.com>
2024-06-07 22:33:47 -0500

    Added multiple math functions like floor,round, and prime finding

--- problems/Math-Library/pe-utils.h
+++ problems/Math-Library/pe-utils.h
@@ -1,9 +1,22 @@
+#include <stdbool.h>
 #ifndef PE_UTILS_H
 #define PE_UTILS_H
 
 // Function declarations
 int power(int x, int exponent);
 
-int nextPrime(int x);
+bool isPrime(long long start);
+
+long long nextPrime(long long start);
+
+long long absoluteValue(long long start);
+
+float absoluteFloat(long long start);
+
+long long roundValue(float start);
+
+long long floorValue(float start);
+
+float squareRoot(long long start);
 
 #endif
