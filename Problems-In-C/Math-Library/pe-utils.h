e0626ba2fae8013f64ce541036ba0db43d0e0028
Alex Ocegueda Castro <alexocegueda59@gmail.com>
2024-07-09 18:05:16 -0500

    Renamed folder for C Files

---
+++ Problems-In-C/Math-Library/pe-utils.h
@@ -0,0 +1,22 @@
+#include <stdbool.h>
+#ifndef PE_UTILS_H
+#define PE_UTILS_H
+
+// Function declarations
+int power(int x, int exponent);
+
+bool isPrime(long long start);
+
+long long nextPrime(long long start);
+
+long long absoluteValue(long long start);
+
+float absoluteFloat(long long start);
+
+long long roundValue(float start);
+
+long long floorValue(float start);
+
+float squareRoot(long long start);
+
+#endif
